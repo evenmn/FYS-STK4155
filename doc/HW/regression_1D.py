dfebd779e41271c1634819a0028017047dcb60c6
evenmn <evenmn@fys.uio.no>
2018-09-19 14:38:58 +0200

    Added regression classes for 1D and 2D

---
+++ doc/HW/regression_1D.py
@@ -0,0 +1,114 @@
+import numpy as np
+import matplotlib.pyplot as plt
+from random import random
+from tqdm import tqdm
+
+
+class Reg_1D():
+
+    def __init__(self, x, y, p):
+        '''
+        Arguments:
+        ----------
+        
+        x:      Numpy array.
+                X-component of all points.
+                
+        y:      Numpy array.
+                Y-component of all points.
+                
+        n:      Integer.
+                Order of fitting polynomial.'''
+    
+        self.x = x
+        self.y = y
+        self.p = p
+
+
+    def ols(self):
+        '''Ordinary Least Square (OLS) regression'''
+        
+        x = self.x
+        y = self.y
+        p = self.p
+        
+        xb = np.c_[np.ones((len(x),1))]
+        for i in range(1,p+1):
+            xb = np.c_[xb, x**i]
+
+        beta = np.linalg.inv(xb.T.dot(xb)).dot(xb.T).dot(y)
+        return beta.flatten()[::-1]
+
+
+    def reg_q(self, q, λ=0.1, η=0.0001, niter=100000):
+        '''Generalized form of Ridge and Lasso regression
+        
+        Arguments:
+        ----------
+                
+        q:      Float.
+                Degree of additional element.
+                Lasso: q=1
+                Ridge: q=2
+                SLR:   q=-∞.
+                
+        eta:    Float.
+                Learning rate.
+                
+        niter:  Integer.
+                Number of iterations in gradient descent.'''
+        
+        x = self.x
+        y = self.y
+        p = self.p
+        
+        # Setting up X-matrix
+        xb = np.c_[np.ones((len(x), 1))]
+        for i in range(1, p+1):
+            xb = np.c_[xb, x**i]
+
+        # Minimization using gradient descent
+        beta = np.random.randn(p+1, 1)
+        for iter in tqdm(range(niter)):
+            e = y - xb.dot(beta)                    # Absolute error
+            beta += η*(2*xb.T.dot(e) - q*λ*np.power(abs(beta), q-1))
+        
+        return beta.flatten()[::-1]
+
+
+    def ridge(self, λ=0.1, η=0.0001, niter=100000):
+        '''Ridge regression'''
+        return Reg_1D.reg_q(self, 2, λ, η, niter)
+        
+        
+    def lasso(self, λ=0.1, η=0.0001, niter=100000):
+        '''Lasso regression'''
+        return Reg_1D.reg_q(self, 1, λ, η, niter)
+        
+
+def f(x, pol):
+    '''Polyval'''
+    return np.polyval(pol, x)
+    
+    
+if __name__ == '__main__':
+    x_vals     = np.linspace(0,2,100)
+    x          = 2*np.random.rand(100,1)
+    y          = 4+3*x+np.random.randn(100,1)
+    
+    order2     = Reg_1D(x, y, 2)
+    beta_ols   = order2.ols()
+    beta_ridge = order2.ridge()
+    beta_lasso = order2.lasso()
+
+    
+    #print(beta_ols)
+    #print(beta_ridge)
+    #print(beta_lasso)
+
+    plt.plot(x, y, '.')
+    plt.plot(x_vals, f(x_vals, beta_ols), label='OLS')
+    plt.plot(x_vals, f(x_vals, beta_ridge), label='ridge')
+    plt.plot(x_vals, f(x_vals, beta_lasso), label='lasso')
+    plt.legend(loc='best')
+    plt.show()
---
+++ doc/Project1/scripts/regression_2D.py
@@ -0,0 +1,209 @@
+import numpy as np
+import matplotlib.pyplot as plt
+from numpy.random import uniform, normal
+from tqdm import tqdm
+from franke import FrankeFunction
+
+# Matplotlib stuff
+from mpl_toolkits.mplot3d import Axes3D
+from matplotlib import cm 
+from matplotlib.ticker import LinearLocator, FormatStrFormatter
+
+
+class Reg_2D():
+
+    def __init__(self, x, y, z, Px, Py):
+        '''
+        Arguments:
+        ----------
+        
+        x:      Numpy array.
+                X-component of all points.
+                
+        y:      Numpy array.
+                Y-component of all points.
+                
+        z:      Numpy array.
+                Z-component of all points.
+                
+        Px:     Integer.
+                Order of fitting polynomial in x-direction.
+                
+        Py:     Integer.
+                Order of fitting polynomial in y-direction.'''
+                
+        self.x = x
+        self.y = y
+        self.z = z
+        self.Px = Px
+        self.Py = Py
+
+
+    def ols(self):
+        '''Regression, finding coefficients beta'''
+        
+        x = self.x
+        y = self.y
+        z = self.z
+        Px = self.Px
+        Py = self.Py
+
+        # Setting up x-matrix
+        Px = Px+1; Py = Py+1
+        X = np.zeros([N, Px*Py])
+        for i in range(N):
+            for j in range(Px):
+                for k in range(Py):
+                    X[i,Py*j+k] = x[i]**j*y[i]**k
+
+        # Calculating beta-vector
+        beta = np.linalg.inv(X.T.dot(X)).dot(X.T).dot(z)
+        return np.reshape(beta.flatten(), (Px,Py))
+
+
+    def reg_q(self, q, λ=0.1, η=0.0001, niter=1000000):
+        '''Regression, finding coefficients beta
+        
+        Arguments:
+        ----------
+        
+        q:      Integer.
+                Exponent in last term.
+                
+        λ:      Float.
+                Penalty.
+        
+        η:      Float.
+                Learning rate.
+                
+        niter:  Integer.
+                Number of iterations in gradient descent.'''
+                
+        x = self.x
+        y = self.y
+        z = self.z
+        Px = self.Px
+        Py = self.Py 
+
+        # Setting up x-matrix
+        Px = Px+1; Py = Py+1
+        X = np.zeros([N, Px*Py])
+        for i in range(N):
+            for j in range(Px):
+                for k in range(Py):
+                    X[i,Py*j+k] = x[i]**j*y[i]**k
+        
+
+        # Minimization using gradient descent
+        beta = np.random.randn(Px*Py, 1)
+
+        for iter in tqdm(range(niter)):
+            e = y - X.dot(beta).T                    # Absolute error
+            beta += η*(2*X.T.dot(e.T) - q*λ*np.power(abs(beta), q-1))
+        
+        return np.reshape(beta.flatten(), (Px,Py))
+        
+        
+    def ridge(self, λ=0.1, η=0.0001, niter=1000000):
+        '''Ridge regression'''
+        return Reg_2D.reg_q(self, 2, λ, η, niter)
+        
+        
+    def lasso(self, λ=0.1, η=0.0001, niter=1000000):
+        '''Lasso regression'''
+        return Reg_2D.reg_q(self, 1, λ, η, niter)
+
+
+def polyval(x, y, beta):
+    '''Polyval'''
+    
+    if isinstance(x, np.ndarray):
+        z = np.zeros(shape=x.shape)
+    else:
+        z = 0
+        
+    for i in range(beta.shape[0]):
+        for j in range(beta.shape[1]):
+            z += beta[i,j]*np.multiply(np.power(x,i), np.power(y,j))
+    return z
+    
+    
+
+if __name__ == '__main__':
+    N  = 100        # Number of points
+    D  = 2          # Dimension
+    Px = 5          # Polynomial order in x-direction
+    Py = 5          # Polynomial order in y-direction
+
+    noise = normal(0,0.1,N)
+
+    x = uniform(0,1,N)
+    y = uniform(0,1,N)
+    z = FrankeFunction(x, y) + noise
+    
+    order5 = Reg_2D(x, y, z, Px, Py)
+    
+    beta_ols = order5.ols()
+    beta_ridge = order5.ridge()
+    beta_lasso = order5.lasso()
+    
+    #plt.imshow(beta_ols)
+    #plt.show()
+    
+    #plt.imshow(beta_ridge)
+    #plt.show()
+    
+    #plt.imshow(beta_lasso)
+    #plt.show()
+    
+    
+    x_vals = y_vals = np.linspace(0,1,1000)
+    X_vals, Y_vals = np.meshgrid(x_vals, y_vals)
+    predict = polyval(X_vals, Y_vals, beta_ols)
+    
+    # PLOT
+    fig = plt.figure() 
+    ax = fig.gca(projection='3d')
+    
+    #Plot the surface. 
+    surf = ax.plot_surface(X_vals,Y_vals,predict,cmap=cm.coolwarm,linewidth=0,antialiased=False)
+    #surf = ax.plot_surface(X_vals,Y_vals,FrankeFunction(X_vals, Y_vals),cmap=cm.coolwarm,linewidth=0,antialiased=False)
+
+    #Customize the z axis. 
+    ax.set_zlim(-0.10,1.40)
+    ax.zaxis.set_major_locator(LinearLocator(10))
+    ax.zaxis.set_major_formatter(FormatStrFormatter('%.02f'))
+    ax.set_xlabel('X axis')
+    ax.set_ylabel('Y axis')
+    ax.set_zlabel('Z axis')
+
+    #Add a color bar which maps values to colors. 
+    fig.colorbar(surf, shrink=0.5, aspect=5)
+    plt.show()
+    
+    
+    # 1D plot
+    y_const = 0.5
+    z = FrankeFunction(x, y_const) + noise
+    
+    plt.plot(x, z, '.', label='Points')
+    plt.plot(x_vals, polyval(x_vals, y_const, beta_ols), label='Fitted')
+    plt.plot(x_vals, FrankeFunction(x_vals, y_const), label='Franke')
+    plt.legend(loc='best')
+    plt.show()
+    
+    
+    
+    
+    # Confidence interval, beta
+    print(np.var(beta_ols))
+    
+    
+    # Mean square error (MSE):
+    MSE = (z - polyval(x, y, beta_ols)).T.dot(z - polyval(x, y, beta_ols))/N
+    print(MSE)
+    
+    
+    # R2 score function
+    denominator = (y-np.mean(y)).T.dot(y-np.mean(y))
+    print(1-N*MSE/denominator)
