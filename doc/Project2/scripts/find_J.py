4440c9f1ea6799cd462bc6b7d2755d5e27482d83
evenmn <evenmn@fys.uio.no>
2018-10-11 15:05:11 +0200

    Improved performance of state sampling and added temperature dependence

--- doc/Project2/scripts/find_J.py
+++ doc/Project2/scripts/find_J.py
@@ -4,28 +4,32 @@ from ising_data import *
 from regression import *
 
 # define Ising model params
-L=4         # system size
-N=1000000   # Number of states
+L = 100         # System size
+N = 100000   # Number of states
+T = 100       # Temperature
 
 # create random Ising states
-states = np.random.choice([-1, 1], size=(N, L))
+states = produce_states([N, L], T)
 X = np.multiply(states[:,1:], states[:,:-1])
 
 # calculate Ising energies
-E = ising_energies(states, L)
+E = ising_energies(states)
+
+print(X)
+print(E)
 
 model = Reg(X, E)
 
-J_ols = model.ols()
+#J_ols = model.ols()
 J_ridge = model.ridge()
-J_lasso = model.lasso()
+#J_lasso = model.lasso()
 
-J_list = ['J_ols', 'J_ridge', 'J_lasso']
+J_list = ['J_ridge'] #['J_ols', 'J_ridge', 'J_lasso']
 
 for J in J_list:
     J_eval = eval(J)
     
     print('\n--- ', J, ' ---')
     print(J_eval)
-    print('MSE: ', (J_eval+1).dot(J_eval+1)/len(L))
-    print('R2: ', (J_eval+1).dot(J_eval+1)/(J_eval+1).dot(J_eval+1)
+    print('MSE: ', (J_eval+1).dot(J_eval+1)/L)
+    #print('R2: ', (J_eval+1).dot(J_eval+1)/(J_eval+1).dot(J_eval+1))
--- doc/Project2/scripts/ising_data.py
+++ doc/Project2/scripts/ising_data.py
@@ -1,16 +1,40 @@
 import numpy as np
 
-def ising_energies(states, L, J_const=1.0):
+def ising_energies(states, J_const=1.0):
     """
     This function calculates the energies of the states in the nn Ising Hamiltonian
     """
-    J=np.zeros((L,L),)
-    for i in range(L):
-        J[i,(i+1)%L]-=J_const
-    # compute energies
-    E = np.einsum('...i,ij,...j->...',states,J,states)
+    
+    X = np.multiply(states[:,1:], states[:,:-1])
+    J = np.full(X.shape[1], J_const)
 
-    return E
+    return X.dot(J)
+    
+    
+def P_up(T):
+    '''Probability of spin down'''
+    return 1/(1+np.exp(2/T))
+    
+    
+def P_dn(T):
+    '''Probability of spin up'''
+    return 1/(1+np.exp(-2/T))
+    
+    
+def produce_states(size, T):
+    '''Producing Ising states
+    
+    Arguments:
+    ----------
+    
+    size:   List/scalar.
+            Size of system.
+            
+    T:      Scalar.
+            Dimensionless temperature
+            T'=kT/E. '''
+            
+    return np.random.choice([1,-1], size, p=[P_up(T), P_dn(T)])
     
     
 if __name__ == '__main__':
@@ -18,16 +42,13 @@ if __name__ == '__main__':
     
     # Simple example
     
-    # define Ising model params
-    L=5         # system size
-    N=5     # Number of states
+    L=4     # System size
+    N=4     # Number of states
 
-    # create random Ising states
-    states=np.random.choice([-1, 1], size=(N,L))
+    states = produce_states([L, N], T=100)
 
     # calculate Ising energies
-    energies=ising_energies(states,L)
+    energies = ising_energies(states)
 
     print(states)
     print(energies)
-
