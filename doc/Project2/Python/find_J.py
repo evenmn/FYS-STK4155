2134390416b0fc9dd348d39e565b652a483a31b9
evenmn <evenmn@fys.uio.no>
2018-11-05 17:06:13 +0100

    Producing J plots correctly, and adding them to report

--- doc/Project2/Python/find_J.py
+++ doc/Project2/Python/find_J.py
@@ -10,60 +10,65 @@ from error_tools import *
 L = 40         # System size
 N = 10000      # Number of states
 T = 1000         # Temperature
+path = '../plots/'
 
-# create random Ising states
+# Generate random Ising states
 states = produce_states([N, L], T)
 
 X=np.einsum('...i,...j->...ij', states, states) # Outer product of each state
 shape=X.shape
 X=X.reshape((shape[0],shape[1]*shape[2]))       # Flatten along 0/1-axis
 
-# calculate Ising energies
+# Calculate corresponding Ising energies
 J = generate_J(L)
 E = ising_energies(states,J)
 
 n = int(4*N/5)                                  # Number of training states
-λ = 0.001
 
-'''
-# Linear regression
-ols=linear_model.LinearRegression()
-ols.fit(X[:n], E[:n])
-J_ols = ols.coef_
+for λ in [1e-1, 1e-2, 1e-3, 1e-4, 1e-5, 1e-6]:
 
-ridge=linear_model.Ridge()
-ridge.set_params(alpha=λ)
-ridge.fit(X[:n], E[:n])
-J_ridge = ridge.coef_
+    # Linear regression
+    ols=linear_model.LinearRegression()
+    ols.fit(X[:n], E[:n])
+    J_ols = ols.coef_
 
-lasso=linear_model.Lasso()
-lasso.set_params(alpha=λ)
-lasso.fit(X[:n], E[:n])
-J_lasso = lasso.coef_
+    ridge=linear_model.Ridge()
+    ridge.set_params(alpha=λ)
+    ridge.fit(X[:n], E[:n])
+    J_ridge = ridge.coef_
 
-J_list = ['J_ols', 'J_ridge', 'J_lasso']
+    lasso=linear_model.Lasso()
+    lasso.set_params(alpha=λ)
+    lasso.fit(X[:n], E[:n])
+    J_lasso = lasso.coef_
 
-for J_ in J_list:
-    J_eval = eval(J_)
-    
-    print('\n--- ', J_, ' ---')
-    print(J_eval)
-    print('MSE_train: ', MSE_linreg(X[:n], J_eval, E[:n]))
-    print('MSE_test: ', MSE_linreg(X[n:], J_eval, E[n:]))
-    print('R2_train: ', R2(X[:n], J_eval, E[:n]))
-    print('R2_test: ', R2(X[n:], J_eval, E[n:]))
-    
-    MSE_train_kfold, MSE_test_kfold, R2_train_kfold, R2_test_kfold = k_fold(X, E, L, λ, K=10, method=J_)
-    print('MSE_train_kfold: ', MSE_train_kfold)
-    print('MSE_test_kfold: ', MSE_test_kfold)
-    print('R2_train_kfold: ', R2_train_kfold)
-    print('R2_test_kfold: ', R2_test_kfold)
-    
-    J_eval = J_eval.reshape(L,L)
-    plt.imshow(J_eval)
-    plt.title(J_)
-    plt.colorbar()
-    plt.show()
+    J_list = ['J_ols', 'J_ridge', 'J_lasso']
+
+    for J_ in J_list:
+        J_eval = eval(J_)
+        
+        print('\n--- ', J_, ' ---')
+        #print(J_eval)
+        print('MSE_train: ', MSE_linreg(X[:n], J_eval, E[:n]))
+        print('MSE_test: ', MSE_linreg(X[n:], J_eval, E[n:]))
+        print('R2_train: ', R2_linreg(X[:n], J_eval, E[:n]))
+        print('R2_test: ', R2_linreg(X[n:], J_eval, E[n:]))
+        
+        '''
+        MSE_train_kfold, MSE_test_kfold, R2_train_kfold, R2_test_kfold = k_fold(X, E, L, λ, K=10, method=J_)
+        print('MSE_train_kfold: ', MSE_train_kfold)
+        print('MSE_test_kfold: ', MSE_test_kfold)
+        print('R2_train_kfold: ', R2_train_kfold)
+        print('R2_test_kfold: ', R2_test_kfold)
+        '''
+        
+        J_eval = J_eval.reshape(L,L)
+        plt.figure()
+        plt.imshow(J_eval)
+        plt.title(J_ + ' λ = {}'.format(λ))
+        cbar = plt.colorbar(cmap='coolwarm')
+        cbar.ax.tick_params(labelsize=22)
+        plt.savefig(path + J_ + '_lambda_{}.png'.format(int(-np.log10(λ))))
 '''
     
 # Neural network
@@ -74,12 +79,11 @@ W = nn.linear(X[:n], f(E[:n]), 50)
 E_tilde_train = x(nn.recall_linear(X[:n], W))
 E_tilde_test = x(nn.recall_linear(X[n:], W))
 
-
 print('MSE_train: ', MSE(E_tilde_train, E[:n]))
 print('MSE_test: ', MSE(E_tilde_test, E[n:]))
 print('R2_train: ', R2(E_tilde_train, E[:n]))
 print('R2_test: ', R2(E_tilde_test, E[n:]))
-
+'''
 '''
 MSE_train_kfold, MSE_test_kfold, R2_train_kfold, R2_test_kfold = k_fold(X, E, L, λ, K=10, method='J_ols')
 print('MSE_train_kfold: ', MSE_train_kfold)
